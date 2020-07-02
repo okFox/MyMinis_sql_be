873ef265f8d00092795628098a4b488cff77e1ac
okFox <smayfield@gmail.com>
2020-07-01 21:06:12 -0700

    POST PATCH DELETE paints working

--- routes/paints.js
+++ routes/paints.js
@@ -12,4 +12,35 @@ paints.get ('/', async(req, res, next) => {
   }
 });
 
+paints.post('/', async(req, res, next) => {
+  try {
+    const result = await client.query('INSERT INTO paints (name, formula) VALUES ($1, $2) RETURNING *', [req.body.name, req.body.formula]
+    );
+    return res.json(result.rows[0]);
+
+  } catch(err) {
+    return next(err);
+  }
+});
+
+paints.patch('/:id', async(req, res, next) => {
+  try {
+    const result = await client.query('UPDATE paints SET name=$1, formula=$2 WHERE idpaint=$3 RETURNING *', [req.body.name, req.body.formula, req.params.id]
+    );
+    return res.json(result.rows[0]);
+
+  } catch(err) {
+    next(err);
+  }
+});
+
+paints.delete('/:id', async(req, res, next) => {
+  try {
+    await client.query('DELETE from paints WHERE idpaint=$1', [req.params.id]);
+    return res.json({ message: `Paint ${req.params.id} deleted successfully` });
+  } catch(err) {
+    next(err);
+  }
+});
+
 module.exports = paints;
